fbbb333a1dcbdd65f6ff3e743417d3d92ef3b9dc
tau chipurura <tauraichipurura@gmail.com>
2023-05-07 20:27:01 +0200

    registration

--- website/forms.py
+++ website/forms.py
@@ -3,16 +3,15 @@ from django.contrib.auth.models import User
 from django import forms
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label='', widget=forms.TimeField(attrs={'class':'form-control','placeholder':'Email Address'}))
-    first_name = forms.CharField(label='',max_length=100, widget=forms.TimeField(attrs={'class':'form-control','placeholder':'First Name'}))
-    last_name = forms.CharField(label='',max_length=100, widget=forms.TimeField(attrs={'class':'form-control','placeholder':'Last Name'}))
-
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
+    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
     class Meta:
         model = User
         fields = ('username', 'first_name','last_name', 'email', 'password1', 'password2')
 
 
-        def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
          super(SignUpForm, self).__init__(*args, **kwargs)
 
          self.fields['username'].widget.attrs['class'] = 'form-control'
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout #, register
 from django.contrib import messages
-from django.http import HttpResponse
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
@@ -28,6 +28,19 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-
-    return render(request, 'register.html', {})
-    
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #authenticate and log in
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            messages.success(request, 'You have succesfully register: welcome')
+            return redirect('home')
+        else: 
+            form = SignUpForm()
+            return render(request, 'register.html', {'form': form})
+        
+    #return render(request, 'register.html', {'form':form}) 
